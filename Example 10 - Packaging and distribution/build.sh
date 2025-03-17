abd75540db2931b83e1412253a3307962ce4a974
Brian Giffin <bdgiffin@brians-mbp.eduroam-corewlcs-14.okstate.edu>
2025-03-17 17:54:24 -0500

    Updated build

--- Example 10 - Packaging and distribution/build.sh
+++ Example 10 - Packaging and distribution/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 -m pygbag --title "Knight Owl" --app_name "Knight Owl" --icon knight_owl/owl.png --template knight_owl/index.html knight_owl
+python3 -m pygbag --title "Knight Owl" --app_name "Knight Owl" --icon knight_owl/owl.png knight_owl
+#  --template knight_owl/index.html 
